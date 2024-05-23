b06f5254a943dd9a30e11eb7180c1eb65acd7cd5
nakagawa1573 <sosl84@outlook.jp>
2024-05-24 08:56:02 +0900

    test

---
+++ app.vue
@@ -0,0 +1,118 @@
+<template>
+  <div>
+    <header class="side">
+      <img class="logo" src="~/assets/img/logo.png">
+      <div class="item">
+        <img class="item__img" src="~/assets/img/home.png" alt="">
+        <p class="item__txt">
+          <NuxtLink id="index" to="/">
+            ホーム
+          </NuxtLink>
+        </p>
+      </div>
+      <div class="item">
+        <img class="item__img" src="~/assets/img/logout.png" alt="">
+        <button class="item__logout">
+          ログアウト
+        </button>
+      </div>
+    </header>
+    <section class="content">
+      <div class="text__box">
+        <p class="text__ttl">
+          シェア
+        </p>
+        <textarea class="text__input" name="message"></textarea>
+        <button type="button" class="text__btn">
+          シェアする
+        </button>
+      </div>
+      <NuxtPage />
+    </section>
+  </div>
+</template>
+
+<style>
+body {
+  background-color: #14202B;
+}
+
+header {
+  background-color: transparent;
+}
+
+.content {
+  display: flex;
+  height: 100%;
+}
+
+.item__logout {
+  background-color: transparent;
+  border: none;
+  font-size: 14px;
+  color: #fff;
+}
+
+#index {
+  text-decoration: none;
+  color: #fff;
+}
+
+.side {
+  margin-left: 10px;
+  width: 250px;
+}
+
+.logo {
+  width: 100px;
+}
+
+.item {
+  display: flex;
+  margin-bottom: 10px;
+}
+
+.item__img {
+  width: 20px;
+  height: 20px;
+}
+
+.item__txt {
+  margin: 0;
+  color: #fff;
+  font-size: 14px;
+  margin-left: 10px;
+  line-height: 25px;
+}
+
+.text__ttl {
+  color: #fff;
+}
+
+.text__box {
+  width: 100%;
+  max-width: 250px;
+}
+
+.text__input {
+  width: 100%;
+  height: 100px;
+  background-color: transparent;
+  border: 1px solid #fff;
+  border-radius: 6px;
+  resize: none;
+  color: #fff;
+  padding: 5px 10px;
+}
+
+.text__btn {
+  display: block;
+  margin-left: auto;
+  color: #fff;
+  background-color: #5419DA;
+  height: 35px;
+  width: 90px;
+  border-radius: 999px;
+  font-size: 12px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,39 @@
+<template>
+    <section class="message">
+        <table class="message__box">
+            <tr class="message__header--row">
+                <th class="message__header">
+                    ホーム
+                </th>
+            </tr>
+            <tr class="message__item">
+                <td class="message__data">
+                    <div class="message__item--box">
+                        <span class="message__item--name">
+                            ユーザーネーム
+                        </span>
+                        <div>
+                            <img class="message__item--img" src="~/assets/img/heart.png">
+                            <span>
+                                0
+                            </span>
+                        </div>
+                        <div>
+                            <img class="message__item--img" src="~/assets/img/cross.png">
+                        </div>
+                        <div>
+                            <NuxtLink :to="`/`">
+                                <img class="message__item--img" src="~/assets/img/detail.png">
+                            </NuxtLink>
+                        </div>
+                    </div>
+                    testtest
+                </td>
+            </tr>
+        </table>
+    </section>
+</template>
+
+<style scoped>
+@import '@/assets/css/message.css';
+</style>
